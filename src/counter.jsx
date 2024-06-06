6f3fd01c68bd56ca8c5f85e61626f2ebd5dbdfe1
pranavdevadas <pranavdevadas2@gmail.com>
2024-06-06 16:58:55 +0530

    Counter project

---
+++ src/counter.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+function Counter() {
+    let [count, setCount] = useState(0)
+    return (
+        <div>
+            <h1>Count : {count} </h1>
+            <button onClick={() => setCount(count += 1)}>Increase</button> &nbsp;&nbsp;&nbsp;
+            <button onClick={() => setCount(count -= 1)}>Decrease</button>
+        </div>
+    )
+}
+
+export default Counter
+
+
